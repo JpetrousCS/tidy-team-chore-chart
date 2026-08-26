53760c457dd40c94b636d3e7741255deb2b5f678
JpetrousCS <jonathan@jpetrous.com>
2026-08-26 19:28:26 -0400

    Add synchronized reward deductions and visual star math

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./kid-logins.css";
+import "./reward-math.css";
 import { PwaRegister } from "./pwa-register";
 
 export const metadata: Metadata = {
